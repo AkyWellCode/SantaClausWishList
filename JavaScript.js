1dc2b04e050898b37678fbf0cd48603967cd4d08
Nicula Dragos <110388376+AkyWellCode@users.noreply.github.com>
2022-08-03 16:28:59 +0300

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -9,16 +9,48 @@ function wishAdded() {
 }
 
 function search() {
-  var wish = $('#searchWish').val(), stop = 0;
+  var wish = $('#searchWish').val(), stop = 0, id = 1;
   for (var i = 0; i < wishNumber && stop == 0; ++i) {
     var position = i, checkThisWish = list.get(position);
     if (checkThisWish == wish) {
-      alert("Your wish is on Santa's list!");
+      $('#1').remove();
+      $('#wishCheck').append(`
+        <div id="1">
+          <div class="checkBoxAnswear"
+              <p class="font" id="+ id +" style="color:blue; font-size:35px;">Your wish is on Santa's list!</p>
+          </div>
+        </div>
+        `);
       stop = 1;
     }
   }
   if (stop == 0) {
-    alert("Your wish is missing from Santa's list");
+    $('#1').remove();
+    $('#wishCheck').append(`
+      <div id="1">
+        <div class="checkBoxAnswear"
+          <p class="font" id="+ id +" style="color:red; font-size:35px;"> Your wish is not on Santa's list! </p>
+        </div>
+      </div>
+      `);
   }
   return false;
+}
+
+function addListImage() {
+  var wishOne = list.get(0), wishTwo = list.get(1), wishThree = list.get(2);
+  $('#listImage').append(`
+    <div class="imageSpace">
+      <div class="container">
+       <img src="list.jpg" alt="stickyNote" style="width:70%;">
+       <div class="multiLine">
+        <b>Your Wishlist</b>
+
+        `+ wishOne +`
+        `+ wishTwo +`
+        `+ wishThree +`
+       </div>
+     </div>
+    </div>
+  `)
 }
