a0de4b67ef0398e11a37d7e1443eebf8b65207b4
Nicula Dragos <110388376+DragosNicula@users.noreply.github.com>
2022-08-11 15:10:48 +0300

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -11,36 +11,19 @@ function wishAdded() {
 function search() {
   var wish = document.getElementById("searchWish").value, wishCheck = document.getElementById("wishCheck"), stop = 0;
   wishCheck.append(addText);
-  for (var i = 0; i < wishNumber && stop == 0; ++i) {
+  for (let i = 0; i < wishNumber && stop == 0; ++i) {
     var position = i, checkThisWish = list.get(position);
-    if (checkThisWish == wish) {
+    if (checkThisWish === wish) {
       addText.innerHTML = "Your wish is on Santa's list!";
       addText.style.color = "blue";
       addText.style.fontSize = "30px";
       stop = 1;
     }
   }
-  if (stop == 0) {
+  if (stop === 0) {
     addText.innerHTML = "Your wish is not Santa's list!";
     addText.style.color = "red";
     addText.style.fontSize = "30px";
   }
   return false;
-}
-
-function addListImage() {
-  var wishOne = list.get(0), wishTwo = list.get(1), wishThree = list.get(2);
-  $('#listImage').append(`
-    <div class="imageSpace">
-      <div class="container">
-       <img src="list.jpg" alt="stickyNote" style="width:70%;">
-       <div class="multiLine">
-        <b>Your Wishlist</b>
-        `+ wishOne +`
-        `+ wishTwo +`
-        `+ wishThree +`
-       </div>
-     </div>
-    </div>
-  `)
 }
